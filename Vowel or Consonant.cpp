75fdf4a87445f11957799019199f984052091222
sahiljawale <75848702+Sahiljawale@users.noreply.github.com>
2022-02-06 11:50:54 +0530

    Create Vowel or Consonant.cpp

---
+++ Vowel or Consonant.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+  char a;
+  string s="aeiou";
+  cin>>a;
+  bool ans=false;
+  for(int i=0;i<s.size();i++){
+    if (a==s[i]){
+      ans=true;
+      break;
+    }
+  }
+  if(ans)
+    cout<<"Vowel"<<endl;
+  else
+    cout<<"Consonant"<<endl;
+}
+
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
